c92e76ef4e91c964b5407628defc6fff1bff1e9b
Pedro Netto <pedro@pnetto.com>
2025-12-29 16:57:32 +0100

    Add docs to bump.sh

--- bump.sh
+++ bump.sh
@@ -13,8 +13,13 @@ git checkout main
 # Stage all changes
 git add .
 
+# Using npm version
+# npm version patch - Bug fixes and backward-compatible changes. (1.0.1)
+# npm version minor - New features, backward-compatible. (1.1.0)
+# npm version major - Breaking changes, not backward-compatible. (2.0.0)
+
 # --force to allow running with a dirty tree :)
 npm version patch -m "Chore: bump version to %s - $1" --force
 
 # Pushes the current branch and all new tags to GitHub
-git push origin "$BRANCH" --tags
+git push origin HEAD --tags
