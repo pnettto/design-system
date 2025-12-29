10ebe273afbbec5c7b7d75f8762aded1cf407593
Pedro Netto <pedro@pnetto.com>
2025-12-29 16:49:21 +0100

    Chore: bump version to 1.0.2 - Added rounded corners to lightbox image

---
+++ bump.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if the first argument ($1) is empty
+if [ -z "$1" ]; then
+  echo "Error: Missing description."
+  echo "Usage: npm run bump \"A description here\""
+  exit 1
+fi
+
+# Make sure we're on the main branch and not in a detached HEAD state
+git checkout main
+
+# Stage all changes
+git add .
+
+# --force to allow running with a dirty tree :)
+npm version patch -m "Chore: bump version to %s - $1" --force
