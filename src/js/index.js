1bcbf688c6a9d9c72e159e26114cf91a63f426ea
Pedro Netto <pedro@pnetto.com>
2025-12-30 15:41:57 +0100

    update prism languages

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,18 @@ import { generateImgTag } from "./utils/generateImgTag.js";
 // PrismJS and Python component
 import Prism from "prismjs";
 import "prismjs/components/prism-python";
+import "prismjs/components/prism-yaml";
+import "prismjs/components/prism-nginx";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-docker";
+import "prismjs/components/prism-regex";
+import "prismjs/components/prism-shell-session";
+import "prismjs/components/prism-systemd";
 
 // Plyr CSS and JS
 import Plyr from "plyr";
